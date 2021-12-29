4068caad6d378efe7e1f1a908a1f076dc64ff17f
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2021-12-29 13:27:11 +0300

    feat: api

--- src/utils/mainApi.js
+++ src/utils/mainApi.js
@@ -5,11 +5,99 @@ const _checkResponse = (res) => {
     else return Promise.reject(res.status);
   };
 
-export const register = () => {
+export const register = (name, email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: "POST",
+        credentials: 'include',
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            password,
+        }),
+    }).then(_checkResponse);
+}
+
+export const login = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then(_checkResponse);
+  };
+
+  export const token = () => {
+    return fetch(`${BASE_URL}/users/me`, {
+      method: "GET",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(_checkResponse);
+  };
+
+  export const editUserInfo = (name, email) => {
+    return fetch(`${BASE_URL}/users/me`, {
+      method: "PATCH",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+    }).then(_checkResponse);
+  };
+
+  export const signOut = () => {
+      return fetch(`${BASE_URL}/signout`, {
+        method: "DELETE",
+        credentials: 'include',
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(_checkResponse);
+    };
+
+    export const saveFilm = (film) => {
+      return fetch(`${BASE_URL}/movies`, {
+        method: "POST",
+        credentials: 'include',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+          film
+        ),
+      }).then(_checkResponse);
+    };
+
+    export const deleteFilm = (movieId) => {
+      return fetch(`${BASE_URL}/movies/${movieId}`, {
+        method: "DELETE",
+        credentials: 'include',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ movieId }),
+      }).then(_checkResponse);
+    };
+
+    export const getSavedMovies = () => {
+      return fetch(`${BASE_URL}/movies`, {
+        method: "GET",
+        credentials: 'include',
         headers: {
             "Content-Type": "application/json",
         },
     }).then(_checkResponse);
-}
+    }
